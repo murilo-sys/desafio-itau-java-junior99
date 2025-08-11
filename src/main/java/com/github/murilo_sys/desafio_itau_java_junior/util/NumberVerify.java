3d30563065798b99760aaf83a0ea27e063a52440
unknown <murilod_santiago@hotmail.com>
2025-08-11 19:56:07 -0300

    Otimizando IF

--- src/main/java/com/github/murilo_sys/desafio_itau_java_junior/util/NumberVerify.java
+++ src/main/java/com/github/murilo_sys/desafio_itau_java_junior/util/NumberVerify.java
@@ -4,7 +4,7 @@ import com.github.murilo_sys.desafio_itau_java_junior.exception.BadRequestExcept
 public class NumberVerify {
 
     public static void isValid(Double valor) {
-        if (valor <= 0 || valor == null) throw new BadRequestException("Valor deve ser maior que zero");
+        if (valor == null || valor <= 0) throw new BadRequestException("Valor deve ser maior que zero");
     }
 
 }
