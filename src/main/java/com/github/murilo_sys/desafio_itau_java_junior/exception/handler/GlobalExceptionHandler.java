749c68f3a3e153baeeb1ff5995a2908fec6b578b
unknown <murilod_santiago@hotmail.com>
2025-08-11 19:59:10 -0300

    Adicionando Exception personalizada

--- src/main/java/com/github/murilo_sys/desafio_itau_java_junior/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/github/murilo_sys/desafio_itau_java_junior/exception/handler/GlobalExceptionHandler.java
@@ -1,18 +1,29 @@
 package com.github.murilo_sys.desafio_itau_java_junior.exception.handler;
 
+import com.github.murilo_sys.desafio_itau_java_junior.exception.BadRequestException;
+import com.github.murilo_sys.desafio_itau_java_junior.exception.ExceptionResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(ex.getMessage());
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ExceptionResponse> handleBadRequestException(BadRequestException ex, HttpServletRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                "400",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
 
